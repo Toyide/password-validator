dd683ed9174475a68510104e9a72f4124f349714
Yide <yd907592@dal.ca>
2018-02-10 19:12:10 -0400

    A2

--- app/src/main/java/com/example/yide/password_validator/Validator.java
+++ app/src/main/java/com/example/yide/password_validator/Validator.java
@@ -10,12 +10,8 @@ public class Validator {
     {
         String a = "";
         boolean isAtLeast8   = password.length() >= 8;
-
         boolean justpassword = !password.equals("password");
-        boolean hasUppercase = !password.equals(password.toLowerCase());
-        boolean hasLowercase = !password.equals(password.toUpperCase());
-        if(!hasUppercase) a += "\nMust have at least 1 uppercase character";
-        if(!hasLowercase)a += "\nMust have at least 1 lowercase character";
+
         if(!isAtLeast8)a += "\nMust have more than 8 characters";
         if(!justpassword) a += "\nPassword can not be 'password' ";
         return a;
